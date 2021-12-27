50fe2af0869155f7b0155c387d01d90839c0a235
John Dev <joaoguilhermeofc.contato@gmail.com>
2021-12-26 23:40:19 -0300

    First Commit :tada

---
+++ script.js
@@ -0,0 +1,34 @@
+//variables
+let name = 'Rafael';
+let age = 15;
+let social = 'married';
+let city = 'Fortaleza';
+//array
+let family = ['John', 'Gustavo', 'Mariana']
+console.log(family);
+
+//object
+let people = {
+    name: 'Rafael',
+    age: 15,
+    social: 'married',
+    city: 'Fortaleza',
+};
+console.log(people);
+
+//function
+//realiza uma tarefa e não devolve nada
+function dizerNome(){
+    console.log('John');
+}
+dizerNome();
+
+//realiza uma tarefa e pode ser usada em outras variáveis
+function multiplicar (valor){
+    return valor * 2;
+}
+let resultado = multiplicar(6);
+console.log(resultado);
+
+
+
